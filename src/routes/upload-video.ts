018be27b2c01baf63ccff60f768aa5c4aa10ba99
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-09-13 00:21:42 -0300

    setup eslint + prettier with @rocketseat/eslint-config

--- src/routes/upload-video.ts
+++ src/routes/upload-video.ts
@@ -13,49 +13,54 @@ const pump = promisify(pipeline)
 
 const MB_TO_BYTES = 1_048_576
 
-const FILE_SIZE =  MB_TO_BYTES * 25 // 25Mb
+const FILE_SIZE = MB_TO_BYTES * 25 // 25Mb
 
 export async function uploadVideoRoute(app: FastifyInstance) {
   app.register(fastifyMultipart, {
     limits: {
-      fileSize: FILE_SIZE
-    }
+      fileSize: FILE_SIZE,
+    },
   })
 
   app.post('/uploads', async (req: FastifyRequest, res: FastifyReply) => {
     try {
       const fileData = await req.file()
 
-      if(!fileData) {
+      if (!fileData) {
         return res.status(400).send({ message: 'Missing file input.' })
       }
 
       const fileExtension = path.extname(fileData.filename)
 
-      if(fileExtension !== '.mp3') {
-        return res.status(400).send({ message: 'Invalid input type, please upload a MP3.' })
+      if (fileExtension !== '.mp3') {
+        return res
+          .status(400)
+          .send({ message: 'Invalid input type, please upload a MP3.' })
       }
 
       const fileBaseName = path.basename(fileData.filename, fileExtension)
       const fileUploadName = `${fileBaseName}-${randomUUID()}${fileExtension}`
-      const uploadDestination = path.resolve(__dirname, '../../tmp', fileUploadName)
+      const uploadDestination = path.resolve(
+        __dirname,
+        '../../tmp',
+        fileUploadName,
+      )
 
       await pump(fileData.file, fs.createWriteStream(uploadDestination))
 
       const video = await prisma.video.create({
         data: {
           name: fileData.filename,
-          path: uploadDestination
-        }
+          path: uploadDestination,
+        },
       })
 
       return res.status(200).send({ video })
-      
     } catch (error) {
       if (error instanceof Error) {
         return res.status(400).send({ message: error.message })
       }
-      
+
       return res.status(500).send({ message: 'Internal server error!' })
     }
   })
--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,15 @@ import { getAllPromptsRoute, uploadVideoRoute } from './routes'
 
 const app = fastify()
 
-const port = Number(process.env.PORT) || 3333 
+const port = Number(process.env.PORT) || 3333
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 
-app.listen({
-  port
-}).then(() => {
-  console.log(`HTTP Server running on port: ${port}`)
-})
+app
+  .listen({
+    port,
+  })
+  .then(() => {
+    console.log(`HTTP Server running on port: ${port}`)
+  })
